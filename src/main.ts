49c97c035173c0dc8a82647da2ee38173afac9df
asmilex <andresmm@outlook.com>
2026-01-17 18:48:43 +0100

    remove default settings

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,10 @@
-import {
-	App,
-	Editor,
-	MarkdownView,
-	Modal,
-	Notice,
-	Plugin,
-	file,
-} from "obsidian";
-import {
-	DEFAULT_SETTINGS,
-	MyPluginSettings,
-	SampleSettingTab,
-} from "./settings";
+import { Notice, Plugin } from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
 export default class HelloWorldPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
 		console.debug("Orbnaments loading...");
-
-		await this.loadSettings();
 
 		this.addCommand({
 			id: "remove-syncthing-conflict",
@@ -50,18 +33,6 @@ export default class HelloWorldPlugin extends Plugin {
 		console.debug("Orbnaments unloading");
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			(await this.loadData()) as Partial<MyPluginSettings>,
-		);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-
 	/**
 	 * gather all *.sync-conflict* files in the vault and deletes/trashes them
 	 * @returns the number of files removed
@@ -76,28 +47,11 @@ export default class HelloWorldPlugin extends Plugin {
 			return 0;
 		}
 
-		// Always trash each sync conflict file
 		const promises = syncConflictFiles.map((file) => {
 			return this.app.fileManager.trashFile(file);
 		});
 
 		await Promise.all(promises);
 		return syncConflictFiles.length;
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
